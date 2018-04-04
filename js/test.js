f08b2a34e181c0c8e0e74b3b34ad0de5c474f5a9
JungSuchang <jeongsc1001@gmail.com>
2018-04-04 17:24:01 +0900

    ajax-test1

--- js/test.js
+++ js/test.js
@@ -1,1 +1,12 @@
+/*
 $('h1').addClass('headline');
+*/
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'pages/gallery.html',true);
+xhr.send();
+
+xhr.onload = function() {
+  if (xhr.status == 200 ) {
+    console.log("success");
+  }
+}
