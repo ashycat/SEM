322a0b9c2c3c18837ef72e8819d6acf7d64c6162
JungSuchang <jeongsc1001@gmail.com>
2018-05-24 17:36:26 +0900

    중간저장

---
+++ js/table.js
@@ -0,0 +1,13 @@
+
+
+$("#demo").tabulator({
+  columns:[
+  {title:"Name", field:"name", width:200},
+  {title:"Age", field:"age"},
+  {title:"Gender", field:"gender"},
+  {title:"Height", field:"height", width:80},
+  {title:"Favourite Color", field:"col"},
+  {title:"Date Of Birth", field:"dob"},
+  {title:"Likes Cheese", field:"cheese"},
+  ],
+});
--- js/test.js
+++ js/test.js
@@ -19,3 +19,17 @@ $('nav ul li ul li a').on('click', function(e) {
 	$('#container').remove();
 	$('#contents').load(url).hide().fadeIn('slow');
 });
+
+
+
+$("#demo").tabulator({
+  columns:[
+  {title:"Name", field:"name", width:200},
+  {title:"Age", field:"age"},
+  {title:"Gender", field:"gender"},
+  {title:"Height", field:"height", width:80},
+  {title:"Favourite Color", field:"col"},
+  {title:"Date Of Birth", field:"dob"},
+  {title:"Likes Cheese", field:"cheese"},
+  ],
+});
